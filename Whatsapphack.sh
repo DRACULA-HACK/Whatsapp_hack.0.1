bcad47708d077b58db0404a6e45bb8aa16e7f179
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-04-18 10:08:39 +0530

    Update Whatsapphack.sh

--- Whatsapphack.sh
+++ Whatsapphack.sh
@@ -46,6 +46,8 @@ printf "    \_/\_/  |_| |_|\__,_|\__|___/_| |_|\__,_|\___|_|\_\ v1.0\e[0m\n"
 printf "\n"
 printf "     \e[1;92mAuthor: https://github.com/DRACULA-HACK\n\e[0m"
 printf "\n"
+printf "     \e[1;92mby: MASTER-HACK\n\e[0m"
+printf "\n"
 }
 
 createapp() {
